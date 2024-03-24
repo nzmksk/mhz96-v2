29cbbbc049cacb4c10f313951890e3855eed2a07
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2024-03-24 22:49:00 +0800

    add navbar component

---
+++ app/ui/components/navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import styles from "@/app/ui/home.module.css";
+
+export default function ScrollableNavBar() {
+  // useEffect is necessary to fix "window is not defined" error 
+  useEffect(() => {
+    let prevScrollPosition: number = window.scrollY;
+
+    window.onscroll = () => {
+      const currentScrollPosition: number = window.scrollY;
+      const navbar = document.getElementById(`${styles.navbar}`) as HTMLElement;
+
+      // Hides navbar while scrolling downwards
+      navbar.style.top =
+        prevScrollPosition > currentScrollPosition ? "0" : "-54px";
+
+      prevScrollPosition = currentScrollPosition;
+    };
+  }, []);
+
+  return (
+    <nav id={styles.navbar}>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#skills">Skills</a>
+      <a href="#projects">Projects</a>
+      <a href="#apps">Apps</a>
+      <a href="#blog">Blog</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  );
+}
