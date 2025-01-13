704e8cfbb7fa226f5d0ddee4aa90f358f84f7041
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2025-01-14 00:10:27 +0800

    improve header ui and add twitter link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Linkedin, Github, Twitter } from "lucide-react";
 
 function Header() {
   const sections = ["Home", "Experience", "Skills"];
@@ -10,7 +11,7 @@ function Header() {
           <a href="#home">
             <span className="text-xl text-white font-bold ml-2">
               Mohd Hafiz{" "}
-              <span className="text-slate-400 font-bold hidden sm:inline">
+              <span className="text-slate-400 font-bold hidden md:inline">
                 Zabba
               </span>
             </span>
@@ -20,29 +21,38 @@ function Header() {
         <div className="hidden sm:flex flex-row space-x-4 text-white">
           {sections.map((section) => {
             return (
-                <a
+              <a
                 className="group transition-all duration-300 ease-in-out"
                 href={`#${section.toLowerCase()}`}
                 key={section}
-                >
+              >
                 <span className="bg-left-bottom bg-gradient-to-r from-slate-400 to-white bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                   {section}
                 </span>
-                </a>
+              </a>
             );
           })}
         </div>
 
         <div className="flex space-x-4 items-center">
-          <button className="hidden md:inline-block text-white bg-blue-700 hover:bg-blue-500 px-4 py-2 rounded-lg hover:shadow-lg">
+          <button className="border border-slate-900 text-white bg-blue-700 hover:bg-blue-500 px-4 py-2 rounded-lg hover:shadow-lg">
             <a href="https://www.linkedin.com/in/mhz96/" target="_blank">
-              LinkedIn
+              <Linkedin className="inline" />
+              <span className="hidden lg:inline lg:ml-2">LinkedIn</span>
+            </a>
+          </button>
+
+          <button className="hidden sm:inline border border-slate-900 text-white bg-sky-600 hover:bg-sky-400 px-4 py-2 rounded-lg hover:shadow-lg">
+            <a href="https://x.com/mssngnnja" target="_blank">
+              <Twitter className="inline" />
+              <span className="hidden lg:inline lg:ml-2">Twitter</span>
             </a>
           </button>
 
           <button className="border text-white border-slate-900 hover:bg-slate-700 px-4 py-2 rounded-lg hover:shadow-lg">
             <a href="https://github.com/nzmksk" target="_blank">
-              GitHub
+              <Github className="inline" />
+              <span className="hidden lg:inline lg:ml-2">GitHub</span>
             </a>
           </button>
         </div>
